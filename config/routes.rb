6363da191c3fae1270bc9f972520833d816dbe1f
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-06-08 12:23:56 -0400

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ module HanamiCockroachdb
   class Routes < Hanami::Routes
     root { "Hello from Hanami" }
     get "/tasks", to: "tasks.index"
+    post "/tasks", to: "tasks.create"
+    get "/tasks/:id", to: "tasks.show"
+    patch "/tasks/:id", to: "tasks.update"
+    delete "/tasks/:id", to: "tasks.destroy"
   end
 end
