67751bac07b1feeb448fe8178ccdf9014a140341
Carlos Armando Marcano Vargas <cm.games2704@gmail.com>
2023-06-11 23:08:44 -0400

    Update update_spec.rb

--- spec/requests/update_spec.rb
+++ spec/requests/update_spec.rb
@@ -2,17 +2,15 @@
 
 RSpec.describe "PATCH /tasks/:id", type: [:request, :database] do
     let(:task) { app["persistence.rom"].relations[:tasks] }
-    let(:request_headers) do
-      {"HTTP_ACCEPT" => "application/json", "CONTENT_TYPE" => "application/json"}
+    let!(:id) do
+      task.insert(task: "Publish a new article", completed: "false")
     end
   
     context "when a task matches the given id" do
-        let!(:id) do
-          task.insert(task: "Publish a new article", completed: "false")
-        end
+        
     
         it "renders the task" do
-          get "/tasks/#{id}"
+          patch "/tasks/#{id}", {"task": {"task":"Publish a new article", "completed":"false"}}.to_json, "CONTENT_TYPE" => "application/json"
     
           expect(last_response).to be_successful
           expect(last_response.content_type).to eq("application/json; charset=utf-8")
@@ -26,42 +24,40 @@ RSpec.describe "PATCH /tasks/:id", type: [:request, :database] do
       end
 
     context "given valid params" do
-      let!(:id) do
-        task.insert(task: "Publish a new article", completed: "false")
-      end
+      
       it "should update the task" do
-        patch "/tasks/#{id}", params: { task: "New Title" }
+        patch "/tasks/#{id}", {"task": {"task":"Update task", "completed":"true"}}.to_json, "CONTENT_TYPE" => "application/json"
   
-        expect(response).to be_successful
-        expect(task.reload.title).to eq("New Title")
+        expect(last_response).to be_successful
+        updated_task = task.by_pk(id).one
+
+        expect(updated_task[:task]).to eq("Update task")
+        expect(updated_task[:completed]).to eq("true")
       end
-    end  
-  
+    end
+    
     context "given invalid params" do
-      let(:params) do
-        {task: {task: nil}}
-      end
+     
   
       it "returns 422 unprocessable" do
-        patch "/tasks/#{id}", params.to_json, request_headers
+        patch "/tasks/#{id}", {task: {task: nil}}.to_json, "CONTENT_TYPE" => "application/json"
   
         expect(last_response).to be_unprocessable
       end
     end
 
     context "when no task matches the given id" do
-        it "returns not found" do
-          patch "/tasks/#{task.max(:id).to_i + 1}"
-    
-          expect(last_response).to be_not_found
-          expect(last_response.content_type).to eq("application/json; charset=utf-8")
-    
-          response_body = JSON.parse(last_response.body)
-    
-          expect(response_body).to eq(
-            "error" => "not_found"
-          )
-        end
+      it "returns not found" do
+        patch "/tasks/#{task.max(:id).to_i + 1}", {"task": {"task":"Update task", "completed":"true"}}.to_json, "CONTENT_TYPE" => "application/json"
+  
+        expect(last_response).to be_not_found
+  
+        response_body = JSON.parse(last_response.body)
+  
+        expect(response_body).to eq(
+          "error" => "not_found"
+        )
+      end
     end
 end
   
